68f4c6788e8069a8f1709cce2d7b248ac3b5ad3d
Artem <goldtree3000@gmail.com>
2023-12-23 00:57:23 +0300

    feat: solve vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  constructor(reverse = true) {
+    this.reverse = reverse;
+    this.alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+ }
+
+ encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    this.message = message.toUpperCase();
+    this.key = key.toUpperCase();
+
+
+    let result = "";
+    let index = 0;
+
+    for (let i = 0; i < this.message.length; i++) {
+       if (this.alphabet.includes(this.message[i])) {
+          result += this.alphabet[(((this.message[i].charCodeAt(0) - 65) + (this.key[index].charCodeAt(0) - 65)) % 26)];
+          index++;
+          if (index === this.key.length) index = 0;
+       } else {
+          result += this.message[i];
+       }
+    }
+    return (this.reverse) ? result.split("").join('') : result.split("").reverse().join("");
+ }
+
+ decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+
+    this.encryptedMessage = encryptedMessage.toUpperCase();
+    this.key = key.toUpperCase();
+
+    let result = "";
+    let index = 0;
+
+    for (let i = 0; i < this.encryptedMessage.length; i++) {
+       if (this.alphabet.includes(this.encryptedMessage[i])) {
+          result += this.alphabet[((this.encryptedMessage[i].charCodeAt(0) - 65) - (this.key[index].charCodeAt(0) - 65) + 26) % 26];
+          index++;
+          if (index === this.key.length) index = 0;
+       } else {
+          result += this.encryptedMessage[i];
+       }
+    }
+
+    return (this.reverse) ? result.split("").join('') : result.split("").reverse().join("");
+ }
 }
 
 module.exports = {
